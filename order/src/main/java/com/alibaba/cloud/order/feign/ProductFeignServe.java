6323adfb3036a7c2c77516a2d87cd7bc1f496c7a
lxz <2425220864@qq.com>
2024-10-04 21:24:17 +0800

    build: feign的契约配置，保留feign的原生注解

--- order/src/main/java/com/alibaba/cloud/order/feign/ProductFeignServe.java
+++ order/src/main/java/com/alibaba/cloud/order/feign/ProductFeignServe.java
@@ -1,5 +1,7 @@
 package com.alibaba.cloud.order.feign;
 
+import feign.Param;
+import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "service-product", path = "/product")
 public interface ProductFeignServe {
 
-    @GetMapping("/{id}")
-    String selectProduct(@PathVariable("id") Integer id);
+//    @GetMapping("/{id}")
+//    String selectProduct(@PathVariable("id") Integer id);
+
+    @RequestLine("GET /{id}")
+    String selectProduct(@Param("id") Integer id);
 }
