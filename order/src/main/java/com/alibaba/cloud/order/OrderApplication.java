80add98992dd061f31173ad09fe33785cea307d4
lxz <2425220864@qq.com>
2024-10-04 14:50:48 +0800

    build: 通过添加配置类，消费端服务main方法上添加ribbonClients注解来修改ribbon的负载均衡策略。

--- order/src/main/java/com/alibaba/cloud/order/OrderApplication.java
+++ order/src/main/java/com/alibaba/cloud/order/OrderApplication.java
@@ -1,16 +1,24 @@
 package com.alibaba.cloud.order;
 
+import com.alibaba.cloud.ribbon.RibbonRandomRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @EnableFeignClients
+@RibbonClients(
+        value = {
+                @RibbonClient(name = "service-stock", configuration = RibbonRandomRuleConfig.class)
+        }
+)
 @SpringBootApplication
 public class OrderApplication {
 
