0385b12986d71ef757efe93d7793c0e5bea3e869
lxz <2425220864@qq.com>
2024-10-05 14:36:49 +0800

    feat:(配置中心):配置值获取动态感知

--- order/src/main/java/com/alibaba/cloud/order/controller/OrderController.java
+++ order/src/main/java/com/alibaba/cloud/order/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.alibaba.cloud.order.feign.ProductFeignServe;
 import com.alibaba.cloud.order.feign.StockFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderController {
 
     @Autowired
